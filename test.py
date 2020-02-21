674d1918df196e61066086b0112dad12fd097614
Cl3m69 <1clem69@gmail.com>
2020-02-21 15:16:51 +0100

    sprite planete

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import pygame
-import test2
 pygame.init()
 screen = pygame.display.set_mode((1200,750))
 
 launched = True
 image = pygame.image.load("tets.jpg")
-test2.coucou()
+earth = pygame.image.load("earth.png")
+jupiter = pygame.image.load("jupiter.png")
+mars = pygame.image.load("mars.png")
+sprite = pygame.image.load("spriteplanete.png")
 while launched:
     pygame.display.init()
     screen.blit(image, (0,0))
+    screen.blit(earth, (20,50))
+    screen.blit(jupiter, (120,30))
+    screen.blit(mars, (250,65))
+    screen.blit(sprite, (400,10))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             launched = False
