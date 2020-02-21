63156004bbdfa21ffdb2bf9e4da846ee4cf6a9e6
labrillance <57724244+labrillance@users.noreply.github.com>
2020-02-21 20:45:13 +0100

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((1600,1000))
+
+launched = True
+image = pygame.image.load("tets.jpg")
+while launched:
+    pygame.display.init()
+    screen.blit(image, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            launched = False
+    pygame.display.flip()
