6e4c438086caa27277d8bc8d2cb3d18db10e72cb
labrillance <57724244+labrillance@users.noreply.github.com>
2020-02-22 00:42:59 +0100

    fix bugs

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import widget
 pygame.init()
 screen = pygame.display.set_mode((1600,1000), HWSURFACE | DOUBLEBUF | FULLSCREEN)
 
-icon = pygame.image.load("icon_game.png")
+icon = pygame.image.load("textures/icon_game.png")
 global launched
 launched = False
 global menu_launch
@@ -19,19 +19,18 @@ pygame.display.set_caption("Planet Star")
 
 
 
-image = pygame.image.load("tets.jpg")
-earth = pygame.image.load("earth.png")
-jupiter = pygame.image.load("jupiter.png")
-mars = pygame.image.load("mars.png")
-sprite = pygame.image.load("spriteplanete.png")
-
+image = pygame.image.load("textures/tets.jpg")
+earth = pygame.image.load("textures/earth.png")
+jupiter = pygame.image.load("textures/jupiter.png")
+mars = pygame.image.load("textures/mars.png")
+sprite = pygame.image.load("textures/spriteplanete.png")
+incone_or = pygame.image.load("textures/incone_or.jpg")
+incone_or.convert()
+launched = menu.display_menu(screen, menu_launch)
 
 list = menu.display_menu(screen, menu_launch)
 if (len(list) == 4):
     launched = True
-incone_or = pygame.image.load("incone_or.jpg")
-incone_or.convert()
-launched = menu.display_menu(screen, menu_launch)
 
 while launched:
     pygame.display.init()
