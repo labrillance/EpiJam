0340759f60423dfea0de191db45865e516ac5979
labrillance <57724244+labrillance@users.noreply.github.com>
2020-02-21 20:51:58 +0100

    fix

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import pygame
+import test2
 pygame.init()
 screen = pygame.display.set_mode((1200,750))
 
 launched = True
 image = pygame.image.load("tets.jpg")
+test2.coucou()
 while launched:
     pygame.display.init()
     screen.blit(image, (0,0))
