915ce915364f0da2b759c2d2ec159a96bbfdd185
Mahe <mahe.faure@epitech.eu>
2020-02-23 00:46:12 +0100

    adding size 0 for earth

--- planete.py
+++ planete.py
@@ -9,6 +9,10 @@ def random_planete(taille, posx, posy, long, larg, name):
     classes.planete.y = posy
     classes.planete.long = long
     classes.planete.larg = larg
+    if taille == 0:
+        classes.planete.gold = 35
+        classes.planete.oil = 130
+        classes.planete.iron = 580
     if taille == 1:
         classes.planete.gold = randrange(7, 20)
         classes.planete.oil = randrange(20, 60)
@@ -18,6 +22,6 @@ def random_planete(taille, posx, posy, long, larg, name):
         classes.planete.oil = randrange(60, 180)
         classes.planete.iron = randrange(350, 800)
     if taille == 3:
-        classes.planete.gold = randrange(25, 100)
-        classes.planete.oil = randrange(120, 340)
-        classes.planete.iron = randrange(600, 1200)
+        classes.planete.gold = randrange(35, 100)
+        classes.planete.oil = randrange(150, 340)
+        classes.planete.iron = randrange(650, 1200)
