ffb6fe9d613ec80d341710052b512d6743054f2d
labrillance <57724244+labrillance@users.noreply.github.com>
2020-02-22 03:57:20 +0100

    add a timer for turns

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ incone_or = pygame.image.load("textures/incone_or.jpg")
 incone_or.convert()
 overlay = [pygame.image.load("textures/overlayplayer1.png"), pygame.image.load("textures/overlayplayer2.png"), pygame.image.load("textures/overlayplayer3.png"), pygame.image.load("textures/overlayplayer4.png")]
 overlay = [pygame.transform.scale(overlay[0], (infoObject.current_w, infoObject.current_h)), pygame.transform.scale(overlay[1], (infoObject.current_w, infoObject.current_h)), pygame.transform.scale(overlay[2], (infoObject.current_w, infoObject.current_h)), pygame.transform.scale(overlay[3], (infoObject.current_w, infoObject.current_h))]
-font = pygame.font.Font("./fonts/Andromeda-eR2n.ttf", round((infoObject.current_w * infoObject.current_h * 35 / (1920 * 1080))))
+font = pygame.font.Font("./fonts/Andromeda-eR2n.ttf", round((infoObject.current_w * infoObject.current_h * 45 / (1920 * 1080))))
 
 list = menu.display_menu(screen, menu_launch)
 if (len(list) == 4):
@@ -45,24 +45,37 @@ def init_players(list):
     while i <= 3:
         players.append(classes.player())
         players[i].name = list[i]
-        players[i].name = font.render(players[i].name, True, (255, 255, 255))
+        players[i].name = font.render(players[i].name, True, (0,0,0))
         i += 1
     return players
-
 turn = 0
 players = init_players(list)
+clock_turn = pygame.time.get_ticks()
+seconds = ""
 
 while launched:
     pygame.display.init()
     screen.blit(image, (0,0))
     screen.blit(overlay[turn], (0, 0))
-    screen.blit(players[turn].name, (infoObject.current_w * 12 / 1600, infoObject.current_h * 871 / 1000))
+    screen.blit(players[turn].name, (infoObject.current_w * 20 / 1600, infoObject.current_h * 871 / 1000))
+    seconds = str(int(((20 - (pygame.time.get_ticks() - clock_turn) / 1000))))
+    sec = int(seconds)
+    seconds = font.render(seconds, True, (0,0,0))
+    screen.blit(seconds, (infoObject.current_w * 1520 / 1600, infoObject.current_h * 8 / 1000))
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
             launched = False
-        if event.type == KEYDOWN and event.key == K_RETURN:
+        if (event.type == KEYDOWN and event.key == K_RETURN):
+            clock_turn = pygame.time.get_ticks()
             if turn != 3:
                 turn += 1
             else:
                 turn = 0
+    if sec <= 0:
+        clock_turn = pygame.time.get_ticks()
+        if turn != 3:
+            turn += 1
+        else:
+            turn = 0
+        
     pygame.display.flip()
