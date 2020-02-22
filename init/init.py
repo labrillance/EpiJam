986fec1c48f6f76b2ba5df6bf80d578f41ad25ae
GUICHARD Clément <guichardclement@localhost.localdomain>
2020-02-21 22:07:15 -0500

    classes planete

--- init/init.py
+++ init/init.py
@@ -4,3 +4,15 @@ class player:
         self.gold = 0
         self.oil = 0
         self.iron = 0
+
+class planete:
+    def __init__(self):
+        self.name = ""
+        self.gold = 0
+        self.oil = 0
+        self.iron = 0
+        self.x = 0
+        self.y = 0
+        self.long = 0
+        self.larg = 0
+        self.colonise = 0
